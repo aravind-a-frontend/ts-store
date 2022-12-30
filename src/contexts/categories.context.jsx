dfd0c030a93c3fde52a97af8a9cfb8d792b5ad35
aravind-a-frontend <aravind.a.frontend@gmail.com>
2022-12-30 12:13:34 +0530

    modified:   src/contexts/categories.context.jsx 	modified:   src/routes/category/category.component.jsx

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,16 +1,14 @@
 import { createContext, useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 
 export const CategoriesContext = createContext({
   categories: [],
 });
 
 export const CategoriesProvider = ({ children }) => {
-  const { id } = useParams();
   const [categories, setCategories] = useState([]);
 
   const fetchData = () => {
-    return fetch(`https://fakestoreapi.com/products/category/${id}`)
+    return fetch('https://fakestoreapi.com/products/category/jewelery')
       .then((res) => res.json())
       .then((data) => setCategories(data));
   };
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,25 +1,15 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext } from 'react';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import Spinner from '../../components/spinner/spinner.component';
-
-import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
-  const { category } = useParams();
   const { categories } = useContext(CategoriesContext);
-  const [products, setProducts] = useState();
-
-  useEffect(() => {
-    setProducts(categories[category]);
-  }, [category, categories]);
 
   return (
     <div className="products-container">
-      {products.map((product) => (
+      {categories.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
